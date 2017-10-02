a6b7c2bed8552a2b58d1b7eab7212181846d8b5b
PabloArtero <pabloartero1@gmail.com>
2017-10-02 12:36:24 -0300

    rfef

--- PruebaConflictosGIt/src/pruebaconflictosgit/PruebaConflictosGIt.java
+++ PruebaConflictosGIt/src/pruebaconflictosgit/PruebaConflictosGIt.java
@@ -4,11 +4,6 @@ package pruebaconflictosgit;
 
 public class PruebaConflictosGIt {
 
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> c0d6ef7dfac98767e20a98a3fa738e596b070b72
     public static void main(String[] args) {
         // TODO code application logic here
     }
